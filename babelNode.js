51f4b13a21bb4f652d461582c814e6f6cf4da329
Gabriel Kerneis <gabriel@kerneis.info>
2013-04-08 15:47:43 +0100

    Rewrite server-side in a modular way

---
+++ babelNode.js
@@ -0,0 +1,139 @@
+(function () {
+  "use strict";
+
+  var net = require('net');
+
+  function connect(options) {
+
+    var node = {},
+      state,
+      client,
+      updateCallback = function () { return; };
+
+    function parseBabel(line) {
+      var tokens = line.split(' '), i;
+
+      switch (tokens[0]) {
+      case 'add':
+        /* self is special: it contains no key */
+        if (tokens[1] === "self") {
+          if (typeof state.self.name !== "undefined") {
+            console.error("Duplicate self");
+            return false;
+          }
+          state.self = { name: tokens[2], id: tokens[4] };
+          break;
+        }
+        /* every other kind of add now */
+        if (typeof state[tokens[1]][tokens[2]] === "undefined") {
+          state[tokens[1]][tokens[2]] = {};
+        } else {
+          console.error("Adding a known " + tokens[1] + ": " + tokens[2]);
+          return false;
+        }
+        for (i = 3; i < tokens.length; i += 2) {
+          state[tokens[1]][tokens[2]][tokens[i]] = tokens[i + 1];
+        }
+        break;
+      case 'change':
+        if (typeof state[tokens[1]][tokens[2]] === "undefined") {
+          console.error("Changing a missing " + tokens[1] + ": " + tokens[2]);
+          return false;
+        }
+        for (i = 3; i < tokens.length; i += 2) {
+          state[tokens[1]][tokens[2]][tokens[i]] = tokens[i + 1];
+        }
+        break;
+      case 'flush':
+        if (typeof state[tokens[1]][tokens[2]] === "undefined") {
+          console.error("Flushing a missing " + tokens[1] + ": " + tokens[2]);
+          state[tokens[1]][tokens[2]] = {};
+        } else {
+          delete state[tokens[1]][tokens[2]];
+        }
+        break;
+      case 'BABEL':
+        if (tokens[1] !== '0.0') {
+          console.error('Unknown protocol version: ' + tokens[1]);
+          process.exit(1);
+        }
+        break;
+      default:
+        console.error('Ignoring unknown token: ' + tokens[0]);
+      }
+      return true;
+    }
+
+    function babelConnect() {
+
+      state = {
+        "self": {},
+        "neighbour": {},
+        "xroute": {},
+        "route": {},
+      };
+      /* Store the rest of Babel input, after the latest newline */
+      var buffer = '';
+
+      client = net.connect({port: options.port, host: options.address},
+          function () { console.log("Connected to Babel."); });
+      client.setEncoding('ascii');
+      client.setKeepAlive(true);
+
+      client.on('data', function (data) {
+        var lines = buffer.concat(data).split('\n'), i;
+        buffer = lines.pop();
+        for (i = 0; i < lines.length; i = i + 1) {
+          if (!parseBabel(lines[i])) {
+            console.error("Parse error: reconnecting.");
+            console.error(JSON.stringify(state));
+            /* This will trigger a 'close' event, which reconnects */
+            client.destroy();
+            return;
+          }
+        }
+        updateCallback(node);
+      });
+
+      client.on('close', function (error) {
+        if (error) { return; /* already handled */ }
+        console.error("Babel socket close: reconnecting in 1 second.");
+        setTimeout(babelConnect, 1000);
+      });
+
+      client.on('error', function () {
+        console.error("Babel socket error: reconnecting in 1 second.");
+        setTimeout(babelConnect, 1000);
+      });
+
+    }
+
+    function start() {
+      if (typeof client === "undefined") {
+        babelConnect();
+      } else {
+        /* Trigger a reconnection */
+        client.destroy();
+      }
+    }
+    node.start = start;
+
+    function getState() {
+      return state;
+    }
+    node.getState = getState;
+
+    function on(e, f) {
+      if (e === "update") {
+        updateCallback = f;
+      }
+    }
+    node.on = on;
+
+    return node;
+
+  }
+
+  module.exports.connect = connect;
+
+}());
--- server.js
+++ server.js
@@ -19,200 +19,116 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-var config = {
-    "serverAddress" : "::"       /* Local address to listen on */
-  , "serverPort" : "8080"        /* Local port to listen on */
-  , "babelAddress" : "::1"       /* Babel interface address */
-  , "babelPort" : "33123"        /* Babel interface port */
-  , "updateIval": 3000           /* Time between two updates (ms) */
-  , "verbose": false             /* Print every message received from Babel nodes */
-  , "user": ""                   /* Drop priviledges to this user */
-};
+"use strict";
 
-for(key in config) {
-    var value = process.env["npm_package_config_"+key];
-    if(typeof value != 'undefined' && value != "") {
-        console.log("npm config get babelweb:"+key+" = "+value);
-        config[key] = value;
-    }
+var config = {
+    "serverAddress" : "::",
+    "serverPort" : "8080",
+    "babelAddress" : "::1",
+    "babelPort" : "33123",
+    "updateInterval" : "1000",
+    "verbose": false
+  };
+
+var key;
+for (key in config) {
+  var value = process.env["npm_package_config_" + key];
+  if (typeof value !== 'undefined' && value !== "") {
+    console.log("npm config get babelweb:" + key + " = " + value);
+    config[key] = value;
+  }
 }
 
 process.argv.forEach(function (val, index, array) {
-    if(index >= 2) {
-        var t = val.split('=');
-        if(t.length == 2 && typeof config[t[0]] != 'undefined') {
-            config[t[0]] = t[1];
-        } else {
-            console.error("Warning: couldn't parse option "+val);
-        }
+  if (index >= 2) {
+    var t = val.split('=');
+    if (t.length === 2 && typeof config[t[0]] !== 'undefined') {
+      config[t[0]] = t[1];
+    } else {
+      console.error("Warning: couldn't parse option " + val);
     }
+  }
 });
 
-var babel = {};
-var needUpdate = false;
+
+/* Setup connect */
+var babelNode = require('./babelNode');
+var routers = [
+  babelNode.connect({port: config.babelPort, address: config.babelAddress}),
+// example for a second instance:
+// babelNode.connect({port: parseInt(config.babelPort, 10) + 1, address: config.babelAddress})
+];
+
+function handleUpdate(router) {
+  router.hasChanged = true;
+}
+
+routers.forEach(function (r) {
+  r.on("update", handleUpdate);
+  r.hasChanged = false;
+  r.start();
+});
 
 /* Serving the results */
 
-var http = require('http');
-var net = require('net');
+var connect = require('connect'),
+  http = require('http'),
+  app = connect().use(connect.static(__dirname + '/static')),
+  server = http.createServer(app),
+  io = require('socket.io').listen(server);
 
-var connect = require('connect');
-var server = connect.createServer(connect.static(__dirname + '/static'));
-if(config.verbose)
-        server.use(connect.logger());
+if (config.verbose) { app.use(connect.logger('dev')); } // XXX
+
 server.listen(config.serverPort, config.serverAddress);
 
-var io = require('socket.io').listen(server);
-
-io.configure(function(){
+io.configure(function () {
   io.enable('browser client minification');
   io.enable('browser client etag');
   io.set('log level', config.verbose ? 3 : 1);
 
   io.set('transports', [
-    'websocket'
-  , 'flashsocket' // disabled by default
-  , 'htmlfile'
-  , 'xhr-polling'
-  , 'jsonp-polling'
+    'websocket',
+    // disabled by default
+    'flashsocket',
+    'htmlfile',
+    'xhr-polling',
+    'jsonp-polling'
   ]);
 });
 
 /* Send updates to clients when they connect */
 
-var formatMessage = function () {
- return JSON.stringify( { "table":babel } );
-};
-
-io.sockets.on('connection', function(client){
-    client.send(formatMessage());
+io.sockets.on('connection', function (client) {
+  var states = [];
+  routers.forEach(function (r) {
+    states.push(r.getState());
+  });
+  if (states.length > 0) {
+    client.send(JSON.stringify(states));
+  }
 });
 
-
-/* Monitoring babel */
-
-setInterval(function() {
-    if(needUpdate) {
-        io.sockets.volatile.send(formatMessage());
-        needUpdate = false;
+function timedUpdate() {
+  var update = [];
+  routers.forEach(function (r) {
+    if (r.hasChanged) {
+      update.push(r.getState());
+      r.hasChanged = false;
     }
-}, config.updateIval);
-
-var parseBabel = function(s) {
-    var tokens = s.split(' ');
-
-    /* Work around Babel bug -- now fixed upstream
-       but old nodes might be found in the wild... */
-    if(tokens[1] == "route") {
-        var id = tokens[2].split('-');
-        if(id.length == 3) {
-            tokens[2] = id[0] + "-" + id[2];
-            s = tokens.join(" ");
-        }
-    }
-
-    if(config.verbose)
-        console.error(s);
-
-    switch(tokens[0]) {
-
-        case 'add':
-            if(typeof babel[tokens[1]][tokens[2]] == "undefined") {
-                babel[tokens[1]][tokens[2]] = {};
-            } else {
-                console.error("Adding a known "+tokens[1]+": "+tokens[2]);
-                return false;
-            }
-        case 'change':
-            if(typeof babel[tokens[1]][tokens[2]] == "undefined") {
-                console.error("Changing a missing "+tokens[1]+": "+tokens[2]);
-                return false;
-            }
-            for(var i = 3; i<tokens.length; i += 2)
-                babel[tokens[1]][tokens[2]][tokens[i]] = tokens[i+1];
-            break;
-        case 'flush':
-            if(typeof babel[tokens[1]][tokens[2]] == "undefined") {
-                console.error("Flushing a missing "+tokens[1]+": "+tokens[2]);
-                babel[tokens[1]][tokens[2]] = {};
-            } else {
-                delete babel[tokens[1]][tokens[2]];
-            }
-            break;
-
-        case 'BABEL':
-            if(tokens[1] != '0.0') {
-                console.error('Unknown protocol version: '+tokens[1]);
-                process.exit(1);
-            }
-            break;
-        default:
-            console.error('Ignoring unknown token: ' + tokens[0]);
-    }
-    needUpdate = true;
-    return true;
+  });
+  if (update.length > 0) {
+    io.sockets.volatile.send(JSON.stringify(update));
+  }
+  setTimeout(timedUpdate, config.updateInterval);
 }
 
-var printWorld = function() {
-    console.error(JSON.stringify(babel));
-}
+timedUpdate();
 
-var babelListen = function(port, address) {
-    var client = net.createConnection(port, address);
-    client.setEncoding('ascii');
-    client.setKeepAlive(true);
-    /* Store the rest of Babel input, after the latest newline */
-    var buffer = '';
-
-    babel = {
-        "self": {},
-        "neighbour": {},
-        "xroute": {},
-        "route": {},
-    };
-    needUpdate = true;
-
-    client.addListener('connect', function(){
-        console.error("Connected to Babel.");
-    });
-
-    client.addListener('data', function(data){
-        var lines = buffer.concat(data).split('\n');
-        buffer = lines.pop();
-        for(var i = 0; i < lines.length; i++) {
-            if(!parseBabel(lines[i])) {
-               console.error("Parse error: reconnecting.");
-               printWorld();
-               /* This will trigger a 'close' event, which reconnects */
-               client.destroy();
-               return;
-            }
-        }
-    });
-
-    client.addListener('close', function(error){
-         if(error) { return; /* already handled */ }
-         console.error("Babel socket close: reconnecting in 1 second.");
-         setTimeout(function() {
-             babelListen(port, address);
-         }, 1000);
-    });
-
-    client.addListener('error', function(){
-         console.error("Babel socket error: reconnecting in 1 second.");
-         setTimeout(function() {
-             babelListen(port, address);
-         }, 1000);
-    });
-
-    /* Handle signals */
-    process.on("SIGUSR2", function() {
-        client.destroy();
-    });
-}
-
-process.on("SIGUSR1", function() {
-    printWorld();
+/* Handle signals */
+process.on("SIGUSR2", function () {
+  routers.forEach(function (r) { r.start(); });
+});
+process.on("SIGUSR1", function () {
+  routers.forEach(function (r) { console.log(r.getState()); });
 });
 
-babelListen(config.babelPort, config.babelAddress);
