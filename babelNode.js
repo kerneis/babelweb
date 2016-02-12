6b6751603244ed09a3a229873137dd7c599c9450
Gabriel Kerneis <kerneis@google.com>
2016-02-12 16:43:45 +0100

    Update for local protocol version 1.
    Thanks to Juliusz Chroboczek.

--- babelNode.js
+++ babelNode.js
@@ -6,6 +6,7 @@
   function connect(options) {
 
     var node = {},
+      protocolVersion = 0,
       state,
       client,
       updated,
@@ -13,18 +14,23 @@
       updateCallback = function () { return; },
       closeCallback = function () { return; };
 
-    function parseBabel(line) {
+    function parseBabel(client, line) {
       var tokens = line.split(/\s+/), i;
 
       switch (tokens[0]) {
       case 'add':
         /* self is special: it contains no key */
         if (tokens[1] === "self") {
+          if (protocolVersion > 0) {
+            log("error", "self in version 1");
+            return false;
+          }
           if (typeof state.self.name !== "undefined") {
             log("error", "Duplicate self");
             return false;
           }
-          state.self = { name: tokens[2], id: tokens[4] };
+          state.self.name = tokens[2];
+          state.self.id = tokens[4];
           updated = true;
           break;
         }
@@ -62,13 +68,52 @@
         }
         updated = true;
         break;
+      case 'version':
+        if (typeof state.self.id !== "undefined") {
+          log("error", "Duplicate version");
+          return false;
+        }
+        state.self.version = tokens[1];
+        updated = true;
+        break;
+      case 'host':
+        if (protocolVersion === 0) {
+          log("error", "Unexpected 'host' token in version 0.");
+          return false;
+        }
+        if (typeof state.self.name !== "undefined") {
+          log("error", "Duplicate host");
+          return false;
+        }
+        state.self.name = tokens[1];
+        updated = true;
+        break;
+      case 'my-id':
+        if (protocolVersion === 0) {
+          log("error", "Unexpected 'my-id' token in version 0.");
+          return false;
+        }
+        if (typeof state.self.id !== "undefined") {
+          log("error", "Duplicate my-id");
+          return false;
+        }
+        state.self.id = tokens[1];
+        updated = true;
+        break;
       case 'BABEL':
-        if (tokens[1] !== '0.0') {
+        if (tokens[1].match('^0\.')) {
+          protocolVersion = 0;
+        } else if (tokens[1].match('^1\.')) {
+          protocolVersion = 1;
+          client.write("monitor\n");
+        } else {
           log("error", 'Unknown protocol version: ' + tokens[1]);
           process.exit(1);
         }
         break;
-       case 'done': break;
+      case 'ok':
+      case 'done':
+        break;
       default:
         log("error", 'Ignoring unknown token: ' + tokens[0]);
       }
@@ -79,6 +124,7 @@
 
       state = {
         "self": {},
+        "interface": {},
         "neighbour": {},
         "xroute": {},
         "route": {},
@@ -99,7 +145,7 @@
         var lines = buffer.concat(data).split('\n'), i;
         buffer = lines.pop();
         for (i = 0; i < lines.length; i = i + 1) {
-          if (!parseBabel(lines[i])) {
+          if (!parseBabel(client, lines[i])) {
             log("error", "Parse error: reconnecting.");
             log("error", JSON.stringify(state));
             /* This will trigger a 'close' event, which reconnects */
@@ -112,7 +158,7 @@
       });
 
       client.on('close', function (error) {
-        /* Call close callback only if we got a "self" token */
+        /* Call close callback only if we know the router-id already. */
         if(typeof state.self.id !== "undefined") {
           closeCallback(node, state.self.id);
         }
